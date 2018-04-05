a8caa7df323935ac1e7b50b0ea35741ce67cc5b6
Wesley Kirkland <wesley@wesleyk.me>
2018-04-05 15:54:01 -0400

    Fixed OU generation

--- Examples/Rebuild_AD_OU_Structure.ps1
+++ Examples/Rebuild_AD_OU_Structure.ps1
@@ -6,24 +6,23 @@ $OUs = Get-ADOrganizationalUnit -Filter * |
     Select-Object DistinguishedName,
         Name,
         @{Name='Depth';Expression={$PSItem.DistinguishedName.Split(',').Length}},
-        @{Name='Path';Expression={($OU.DistinguishedName.Split(',').Where{$PSItem -notlike 'DC=*'} | Select-Object -Skip 1) -join ','}} |
+        @{Name='Path';Expression={(($PSItem.DistinguishedName.Split(',').Where{$PSItem.DistinguishedName -notlike 'DC=*'} | Select-Object -Skip 1).Where{$PSItem -notlike "DC=*"}) -join ','}} |
     Sort-Object Depth #Sort on depth so the parent OUs get made first
-
 
 [System.Collections.ArrayList]$FileGeneration = @()
 
 foreach ($OU in $OUs) {
-    if (!(Get-ADOrganizationalUnit -Identity $OU.DistinguishedName)) {
-        $OUPath = "$(
-            if (!([string]::IsNullOrWhiteSpace($OU.Path))) {
-                @(
-                    $($OU.Path)
-                    $($DomainDC)
-                ) -join ','
-            } else {
+    #if (!(Get-ADOrganizationalUnit -Identity $OU.DistinguishedName)) {
+    $OUPath = "$(
+        if (!([string]::IsNullOrWhiteSpace($OU.Path))) {
+            @(
+                $($OU.Path)
                 $($DomainDC)
-            }
-        )"
-        $FileGeneration.Add("New-ADOrganizationalUnit -Name $($OU.Name) -Path $OUPath -ProtectedFromAccidentalDeletion $true") | Out-Null
-    }
+            ) -join ','
+        } else {
+            $($DomainDC)
+        }
+    )"
+    [void]$FileGeneration.Add("New-ADOrganizationalUnit -Name $($OU.Name) -Path '$OUPath' -ProtectedFromAccidentalDeletion $true -ErrorAction SilentlyContinue")
+    #}
 }
