819de30e5b2b7d8c6485c02ba0d965f4afd8e1ec
Wesley Kirkland <wesley@wesleyk.me>
2018-04-05 17:36:32 -0400

    Fixed a few bugs and wil not recreate Domain Controllers

--- Examples/Rebuild_AD_OU_Structure.ps1
+++ Examples/Rebuild_AD_OU_Structure.ps1
@@ -1,9 +1,10 @@
 #This will auto build the Domain Component of the LDAP Path, or you could change it yourself
-$DomainDC = ($Env:USERDNSDOMAIN.ToLower().split('.') | ForEach-Object {"DC=$PSItem"}) -join ','
-#$DomainDC = 'DC=contoso,DC=com'
+#$DomainDC = ($Env:USERDNSDOMAIN.ToLower().split('.') | ForEach-Object {"DC=$PSItem"}) -join ','
+$DomainDC = 'DC=demo1,DC=local'
 $ProtectedFromAccidentalDeletion = $false #Specify the the code below should protect it from accidental deletion, changeable in the script later
 
 $OUs = Get-ADOrganizationalUnit -Filter * |
+    Where-Object Name -NotMatch "Domain Controllers"
     Select-Object DistinguishedName,
         Name,
         @{Name='Depth';Expression={$PSItem.DistinguishedName.Split(',').Length}},
@@ -25,7 +26,7 @@ foreach ($OU in $OUs) {
             $($DomainDC)
         }
     )"
-    [void]$FileGeneration.Add("New-ADOrganizationalUnit -Name $($OU.Name) -Path '$OUPath' -ProtectedFromAccidentalDeletion $ProtectedFromAccidentalDeletion -ErrorAction SilentlyContinue")
+    [void]$FileGeneration.Add("New-ADOrganizationalUnit -Name '$($OU.Name)' -Path '$OUPath' -ProtectedFromAccidentalDeletion $('$ProtectedFromAccidentalDeletion') -ErrorAction SilentlyContinue")
     #}
 }
 
