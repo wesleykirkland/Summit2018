3261ac28d36537d0c6d1f2489aa7b2094ea0d796
Wesley Kirkland <wesley@wesleyk.me>
2018-08-08 22:46:17 -0400

    Update string escaping demo

--- Things_To_Consider_1_Character_Escaping.ps1
+++ Things_To_Consider_1_Character_Escaping.ps1
@@ -1,3 +1,4 @@
+#No escaping
 #Lines 4, 9
 $String = '
 Write-Verbose "$(Join-Path -Path $BinaryPath -ChildPath $BinaryExecutable)"
@@ -14,6 +15,7 @@ if ($SeparateWindow) {
 '
 $String
 
+#Basic string escaping
 $String = '
 Write-Verbose "$(Join-Path -Path $BinaryPath -ChildPath $BinaryExecutable)"
 Write-Verbose "$($Arguments.Trim() -join '' '')"
@@ -27,4 +29,36 @@ if ($SeparateWindow) {
     & "$(Join-Path -Path $BinaryPath -ChildPath $BinaryExecutable)" $Arguments
 }
 '
+$String
+
+#Character escaping Here String
+$String = @'
+Write-Verbose "$(Join-Path -Path $BinaryPath -ChildPath $BinaryExecutable)"
+Write-Verbose "$($Arguments.Trim() -join ' ')"
+
+#Invoke the legacy app
+if ($SeparateWindow) {
+    Write-Verbose "SeparateWindow was invoked, using Start-Process Invocation method"
+    Start-Process -FilePath (Join-Path -Path $BinaryPath -ChildPath $BinaryExecutable) -ArgumentList ($Arguments -join ' ')
+} else {
+    Write-Verbose "Using legacy console invocation method"
+    & "$(Join-Path -Path $BinaryPath -ChildPath $BinaryExecutable)" $Arguments
+}
+'@
+$String
+
+#Character Escaping Script Blocks
+$String = {
+    Write-Verbose "$(Join-Path -Path $BinaryPath -ChildPath $BinaryExecutable)"
+    Write-Verbose "$($Arguments.Trim() -join ' ')"
+    
+    #Invoke the legacy app
+    if ($SeparateWindow) {
+        Write-Verbose "SeparateWindow was invoked, using Start-Process Invocation method"
+        Start-Process -FilePath (Join-Path -Path $BinaryPath -ChildPath $BinaryExecutable) -ArgumentList ($Arguments -join ' ')
+    } else {
+        Write-Verbose "Using legacy console invocation method"
+        & "$(Join-Path -Path $BinaryPath -ChildPath $BinaryExecutable)" $Arguments
+    }
+}.ToString()
 $String
